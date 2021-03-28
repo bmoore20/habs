95d7f853b74b20d2a4dd7f6b02e8cfe03b4e3ac5
Elizabeth Moore <eamoore133@gmail.com>
2021-03-28 00:11:41 -0400

    add file rotating handler and refactor logging

--- hab/train.py
+++ hab/train.py
@@ -10,18 +10,19 @@ import logging
 from hab.dataset import HABsDataset
 from hab.model.model import HABsModelCNN
 from hab.transformations import Rescale, Crop
+from hab.utils import habs_logging
 
 # ------------ logging ------------
-LOGFILE = "./log.log"
-Path(LOGFILE).parent.mkdir(parents=True, exist_ok=True)
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s HABs:%(levelname)s - %(name)s",
+    handlers=[habs_logging.ch, habs_logging.fh]
+)
+
 logger = logging.getLogger(__name__)
-console_handler = logging.StreamHandler()
-file_handler = logging.FileHandler(LOGFILE)
-console_handler.setLevel(logging.INFO)
-file_handler.setLevel(logging.INFO)
-logger.addHandler(console_handler)
-logger.addHandler(file_handler)
+
 # ---------------------------------
+
 
 
 def train(train_data_dir: str, test_data_dir: str):
