187ca45f3542f3da3f2b3fa7cbc9ac0807ab95c9
Elizabeth Moore <eamoore133@gmail.com>
2021-05-18 00:30:56 -0400

    add repr method to CropTimestamp

--- hab/transformations.py
+++ hab/transformations.py
@@ -1,41 +1,16 @@
 from PIL import Image
-from typing import Tuple
 
 
-class Rescale(object):
+class CropTimestamp(object):
     """
-    Rescale image to a specified size.
-    """
-
-    # Referenced: https://pytorch.org/tutorials/beginner/data_loading_tutorial.html
-
-    def __init__(self, output_size: Tuple[int, int]):
-        """
-        Construct instance of a Rescale transformation.
-
-        :param output_size: New image size dimensions.
-        """
-        self.output_size = output_size
-
-    def __call__(self, sample: Image) -> Image:
-        """
-        Perform resizing of image.
-
-        :param sample: Image to be rescaled.
-        :return: Rescaled image.
-        """
-        image = sample.resize(self.output_size)
-
-        return image
-
-
-class Crop(object):
-    """
-    Crop image to remove date and time stamp from bottom of image.
+    Crop image to remove date and timestamp from bottom of image.
     """
 
     def __init__(self):
         ...
+
+    def __repr__(self):
+        return self.__class__.__name__
 
     def __call__(self, sample: Image) -> Image:
         """
