ca7c061552c4ff3577241a2620847138b36e12ab
Elizabeth Moore <eamoore133@gmail.com>
2021-08-09 16:04:06 -0400

    include dataset tests

---
+++ tests/dataset/test_dataset.py
@@ -0,0 +1,22 @@
+import pytest
+
+from hab.dataset import HABsDataset
+from hab.utils.testing_helper import make_transform
+
+
+def test_incorrect_mode_given():
+    with pytest.raises(ValueError):
+        transform = make_transform()
+        image_dataset = "../images/"
+
+        HABsDataset(image_dataset, transform, "error")
+
+
+def test_dataset_train_mode():
+    transform = make_transform()
+    image_dataset = "../images/"
+
+    hab_dataset = HABsDataset(image_dataset, transform)
+    data = hab_dataset[0]
+
+    assert (data[1] == 0) and (len(data) == 2)
